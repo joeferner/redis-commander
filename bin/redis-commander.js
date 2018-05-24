a8ac510addb7f0f8f5a86511bd92019bd94b27f8
Stefan Seide <stefan@trilobyte-se.de>
2018-05-24 19:28:24 +0200

    update dependencies to fix security bugs (expressjs, ejs, ioredis, browserify-middleware, bcrypt)

--- bin/redis-commander.js
+++ bin/redis-commander.js
@@ -70,7 +70,7 @@ var args = optimist
     alias: 'u',
     string: true,
     describe: 'The url prefix to respond on.',
-    default: '',
+    default: ''
   })
   .options('nosave', {
     alias: 'ns',
@@ -153,12 +153,12 @@ myUtils.getConfig(function (err, config) {
       console.log(err);
       process.exit();
     }
-    if (args['sentinel-host'] || args['redis-host'] || args['redis-port'] || args['redis-socket'] || args['redis-password']) {
-      var db = parseInt(args['redis-db']);
-      if (!db) {
+    var db = parseInt(args['redis-db']);
+    if (!db) {
         db = 0;
-      }
+    }
 
+    if (args['sentinel-host'] || args['redis-host'] || args['redis-port'] || args['redis-socket'] || args['redis-password']) {
       newDefault = {
         "label": args['redis-label'] || "local",
         "host": args['redis-host'] || "localhost",
@@ -166,7 +166,7 @@ myUtils.getConfig(function (err, config) {
         "sentinel_port": args['sentinel-port'],
         "port": args['redis-port'] || args['redis-socket'] || "6379",
         "dbIndex": db,
-        "password": args['redis-password'] || '',
+        "password": args['redis-password'] || ''
       };
 
       if (!myUtils.containsConnection(config.default_connections, newDefault)) {
@@ -200,11 +200,7 @@ myUtils.getConfig(function (err, config) {
         }
         setUpConnection(client, db);
       }
-    } else if (config.default_connections.length == 0) {
-      var db = parseInt(args['redis-db']);
-      if (!db) {
-        db = 0
-      }
+    } else if (config.default_connections.length === 0) {
       client = new Redis();
       client.label = args['redis-label'] || "local";
 
