0914acc5900ea1bd99e55500c0e9b2ab65766179
Stefan Seide <account-github@seide.st>
2019-02-14 17:21:29 +0100

    remove hardcoded command list for CommandParser from client side and load supported list from server (list changes if read-only mode is set or not)

--- web/static/scripts/redisCommander.js
+++ web/static/scripts/redisCommander.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var CmdParser = require('cmdparser');
+var cmdparser;
 
 function loadTree () {
   $.get('apiv2/connection', function (isConnected) {
@@ -1019,182 +1020,40 @@ function renderEjs(filename, data, element, callback) {
     });
 }
 
-
-var cmdparser = new CmdParser([
-  "REFRESH",
-
-  "APPEND key value",
-  "AUTH password",
-  "BGREWRITEAOF",
-  "BGSAVE",
-  "BITCOUNT key [start] [end]",
-  "BITOP operation destkey key [key ...]",
-  "BLPOP key [key ...] timeout",
-  "BRPOP key [key ...] timeout",
-  "BRPOPLPUSH source destination timeout",
-  "CONFIG GET parameter",
-  "CONFIG SET parameter value",
-  "CONFIG RESETSTAT",
-  "DBSIZE",
-  "DEBUG OBJECT key",
-  "DEBUG SEGFAULT",
-  "DECR key",
-  "DECRBY key decrement",
-  "DEL key [key ...]",
-  "DISCARD",
-  "DUMP key",
-  "ECHO message",
-  "EVAL script numkeys key [key ...] arg [arg ...]",
-  "EVALSHA sha1 numkeys key [key ...] arg [arg ...]",
-  "EXEC",
-  "EXISTS key",
-  "EXPIRE key seconds",
-  "EXPIREAT key timestamp",
-  "FLUSHALL",
-  "FLUSHDB",
-  "GET key",
-  "GETBIT key offset",
-  "GETRANGE key start end",
-  "GETSET key value",
-  "HDEL key field [field ...]",
-  "HEXISTS key field",
-  "HGET key field",
-  "HGETALL key",
-  "HINCRBY key field increment",
-  "HINCRBYFLOAT key field increment",
-  "HKEYS key",
-  "HLEN key",
-  "HMGET key field [field ...]",
-  "HMSET key field value [field value ...]",
-  "SCAN key cursor [MATCH pattern] [COUNT count]",
-  "HSET key field value",
-  "HSETNX key field value",
-  "HVALS key",
-  "INCR key",
-  "INCRBY key increment",
-  "INCRBYFLOAT key increment",
-  "INFO",
-  "KEYS pattern",
-  "LASTSAVE",
-  "LINDEX key index",
-  "LINSERT key BEFORE|AFTER pivot value",
-  "LLEN key",
-  "LPOP key",
-  "LPUSH key value [value ...]",
-  "LPUSHX key value",
-  "LRANGE key start stop",
-  "LREM key count value",
-  "LSET key index value",
-  "LTRIM key start stop",
-  "MGET key [key ...]",
-  "MIGRATE host port key destination-db timeout",
-  "MONITOR",
-  "MOVE key db",
-  "MSET key value [key value ...]",
-  "MSETNX key value [key value ...]",
-  "MULTI",
-  "OBJECT subcommand [arguments ...]",
-  "PERSIST key",
-  "PEXPIRE key milliseconds",
-  "PEXPIREAT key milliseconds-timestamp",
-  "PING",
-  "PSETEX key milliseconds value",
-  "PSUBSCRIBE pattern [pattern ...]",
-  "PTTL key",
-  "PUBLISH channel message",
-  "PUNSUBSCRIBE [pattern ...]",
-  "QUIT",
-  "RANDOMKEY",
-  "RENAME key newkey",
-  "RENAMENX key newkey",
-  "RESTORE key ttl serialized-value",
-  "RPOP key",
-  "RPOPLPUSH source destination",
-  "RPUSH key value [value ...]",
-  "RPUSHX key value",
-  "SADD key member [member ...]",
-  "SAVE",
-  "SCAN cursor [MATCH pattern] [COUNT count]",
-  "SCARD key",
-  "SCRIPT EXISTS script [script ...]",
-  "SCRIPT FLUSH",
-  "SCRIPT KILL",
-  "SCRIPT LOAD script",
-  "SDIFF key [key ...]",
-  "SDIFFSTORE destination key [key ...]",
-  "SELECT index",
-  "SET key value",
-  "SETBIT key offset value",
-  "SETEX key seconds value",
-  "SETNX key value",
-  "SETRANGE key offset value",
-  "SSCAN key cursor [MATCH pattern] [COUNT count]",
-  "SHUTDOWN [NOSAVE|SAVE]",
-  "SINTER key [key ...]",
-  "SINTERSTORE destination key [key ...]",
-  "SISMEMBER key member",
-  "SLAVEOF host port",
-  "SLOWLOG subcommand [argument]",
-  "SMEMBERS key",
-  "SMOVE source destination member",
-  "SORT key [BY pattern] [LIMIT offset count] [GET pattern [GET pattern ...]] [ASC|DESC] [ALPHA] [STORE destination]",
-  "SPOP key",
-  "SRANDMEMBER key",
-  "SREM key member [member ...]",
-  "STRLEN key",
-  "SUBSCRIBE channel [channel ...]",
-  "SUNION key [key ...]",
-  "SUNIONSTORE destination key [key ...]",
-  "SYNC",
-  "TIME",
-  "TTL key",
-  "TYPE key",
-  "UNSUBSCRIBE [channel ...]",
-  "UNWATCH",
-  "WATCH key [key ...]",
-  "ZADD key score member [score] [member]",
-  "ZCARD key",
-  "ZCOUNT key min max",
-  "ZINCRBY key increment member",
-  "ZINTERSTORE destination numkeys key [key ...] [WEIGHTS weight [weight ...]] [AGGREGATE SUM|MIN|MAX]",
-  "ZLEXCOUNT key min max",
-  "ZRANGE key start stop [WITHSCORES]",
-  "ZRANGEBYLEX key min max [LIMIT offset count]",
-  "ZRANGEBYSCORE key min max [WITHSCORES] [LIMIT offset count]",
-  "ZRANK key member",
-  "ZREM key member [member ...]",
-  "ZREMRANGEBYLEX key min max",
-  "ZREMRANGEBYRANK key start stop",
-  "ZREMRANGEBYSCORE key min max",
-  "ZREVRANGE key start stop [WITHSCORES]",
-  "ZREVRANGEBYLEX key max min [LIMIT offset count]",
-  "ZREVRANGEBYSCORE key max min [WITHSCORES] [LIMIT offset count]",
-  "ZREVRANK key member",
-  "ZSCAN key cursor [MATCH pattern] [COUNT count]",
-  "ZSCORE key member",
-  "ZUNIONSTORE destination numkeys key [key ...] [WEIGHTS weight [weight ...]] [AGGREGATE SUM|MIN|MAX]"
-], {
-  key: function (partial, callback) {
-    var redisConnection = $('#selectedConnection').val();
-    $.get('apiv2/keys/' + encodeURIComponent(redisConnection) + "/" + partial + '*?limit=20', function (data, status) {
-      if (status !== 'success') {
-        return callback(new Error("Could not get keys"));
-      }
-      data = JSON.parse(data)
-        .filter(function (item) {
-          return item.toLowerCase().indexOf(partial.toLowerCase()) === 0;
-        });
-      return callback(null, data);
-    });
-  }
-});
-
 var configTimer;
 var prevSidebarWidth;
 var prevLocked;
 var prevCliHeight;
 var prevCliOpen;
 var configLoaded = false;
+
+
+function initCmdParser() {
+  let parserOpts = {
+    key: function (partial, callback) {
+      var redisConnection = $('#selectedConnection').val();
+      $.get('apiv2/keys/' + encodeURIComponent(redisConnection) + "/" + partial + '*?limit=20', function (data, status) {
+        if (status !== 'success') {
+          return callback(new Error("Could not get keys"));
+        }
+        data = JSON.parse(data)
+        .filter(function (item) {
+          return item.toLowerCase().indexOf(partial.toLowerCase()) === 0;
+        });
+        return callback(null, data);
+      });
+    }
+  };
+
+  $.get('apiv2/redisCommands')
+  .done(function(cmds) {
+    cmdparser = new CmdParser(cmds.list, parserOpts);
+  })
+  .fail(function(error) {
+    console.log('failed to load list of supported redis commands, cannot init CmdParser: ' + JSON.stringify(error));
+    cmdparser = new CmdParser([], parserOpts);
+  });
+}
 
 function getServerInfo (callback) {
   $.get('apiv2/server/info', function (data, status) {
