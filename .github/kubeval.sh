ceb3c6089b25115650f0252bfae765bb80257317
Stefan Seide <account-github@seide.st>
2021-07-27 15:31:06 +0200

    disable debug mode on helm test action

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-set -x
+
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="0.16.1"
 # original schema repo from "instrumenta" not maintained anymore, use the one from "yannh" instead
