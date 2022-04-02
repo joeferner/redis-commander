be30e19db2a3c5c94989799094cb3c1c534648a4
Stefan Seide <account-github@seide.st>
2022-04-02 15:17:37 +0200

    update helm-docs to 1.7.0

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-HELM_DOCS_VERSION="1.5.0"
+HELM_DOCS_VERSION="1.7.0"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
